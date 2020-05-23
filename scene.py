94c26b5182232f5527d30ae6d724cd21cc65e943
Thorben Muhleder <thorbenm@me.com>
2020-05-23 15:12:03 +0100

    Add off and dunkel

--- scene.py
+++ scene.py
@@ -2,16 +2,18 @@
 import sys
 from _phue import set_lights
 
-try:
+
+if len(sys.argv) == 2:
+    t_str = "0.4s"
+else:
     t_str = sys.argv[2]
-    if t_str.endswith("s"):
-        unit_is_minutes = False
-    elif t_str.endswith("m"):
-        unit_is_minutes = True
-    t_str = t_str[:-1]
-    t = float(t_str)
-except:
-    t = .4
+
+
+unit_is_minutes = False
+if t_str.endswith("m"):
+    unit_is_minutes = True
+t_str = t_str[:-1]
+t = float(t_str)
 
 
 if unit_is_minutes:
@@ -23,17 +25,19 @@ t = int(t)
 def hell():
     set_lights(["Hue Go", "Stehlampe", "Fensterlampe"], bri=255, ct=0, time=t)
     set_lights(["Hängelampe"], on=True, time=t)
-    set_lights(["Hängelampe"] ,bri=255, time=t)
+    set_lights(["Hängelampe"], bri=255, time=t)
     set_lights(["Lichterkette"], on=True, time=t)
+
 
 def wakeup():
     hell()
     set_lights(["Tischlampe"], bri=255, ct=0, time=t)
 
+
 def lesen():
     set_lights(["Hue Go", "Stehlampe", "Fensterlampe"], bri=255, ct=500, time=t)
     set_lights(["Hängelampe"], on=True, time=t)
-    set_lights(["Hängelampe"] ,bri=255, time=t)
+    set_lights(["Hängelampe"], bri=255, time=t)
     set_lights(["Lichterkette"], on=True, time=t)
 
 
@@ -41,6 +45,16 @@ def gemutlich():
     set_lights(["Hue Go", "Stehlampe", "Fensterlampe"], bri=153, ct=500, time=t)
     set_lights(["Hängelampe"], on=False, time=t)
     set_lights(["Lichterkette"], on=True, time=t)
+
+
+def dunkel():
+    set_lights(["Hue Go", "Stehlampe", "Fensterlampe"], bri=25, ct=500, time=t)
+    set_lights(["Hängelampe"], on=False, time=t)
+    set_lights(["Lichterkette"], on=True, time=t)
+
+
+def off():
+    set_lights(["Hue Go", "Stehlampe", "Fensterlampe, Hängelampe, Lichterkette, Tischlampe, Schlafzimmer Hängelampe"], on=False)
 
 
 exec(sys.argv[1] + "()")
