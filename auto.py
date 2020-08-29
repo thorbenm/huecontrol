566d461d10c51289dec9ff280464a89dc3dad215
Thorben Muhleder <thorbenm@me.com>
2020-08-29 19:07:31 +0100

    auto function

--- auto.py
+++ auto.py
@@ -7,13 +7,16 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--slaves', type=str, required=True, dest='slaves', help='--slaves="lamp 1,lamp 2"')
 args = parser.parse_args()
 
-master = 'Stehlampe'
-slaves = args.slaves.split(',')
-minimum = 0.1
-maximum = 1.0
+def auto(slaves_string):
+    master = 'Stehlampe'
+    slaves = slaves_string.split(',')
+    minimum = 0.1
+    maximum = 1.0
+    
+    bri = _phue.get_bri(master)
+    bri = max(min(bri, maximum), minimum)
+    ct = _phue.get_ct(master)
+    
+    _phue.set_lights(slaves, bri=bri, ct=ct)
 
-bri = _phue.get_bri(master)
-bri = max(min(bri, maximum), minimum)
-ct = _phue.get_ct(master)
-
-_phue.set_lights(slaves, bri=bri, ct=ct)
+auto(args.slaves)
