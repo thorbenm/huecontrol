5c5a04ca190aab7cb034ddf316e9a8d2759fcf54
Thorben Muhleder <thorbenm@me.com>
2020-10-23 15:12:53 +0100

    Add Comment To start_on_boot.sh

--- start_on_boot.sh
+++ start_on_boot.sh
@@ -1,6 +1,11 @@
 #/bin/bash
+
+# put this into rc.local:
+# sudo -H -u pi /home/pi/Programming/huecontrol/start_on_boot.sh &
+
 THIS_FILE=$(readlink -f "$0")
 THIS_DIR=$(dirname "${THIS_FILE}")
 cd ${THIS_DIR}
 sleep 10
 python3 motionsensor.py
+
