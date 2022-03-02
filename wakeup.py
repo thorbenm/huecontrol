bdb5a8b69d7136fa0a7e8486c00bec4f7bd5f66a
Thorben Muhleder <thorbenm@me.com>
2022-03-02 08:57:22 +0100

    move default transition times into wakeup script

--- wakeup.py
+++ wakeup.py
@@ -20,25 +20,36 @@ def wakeup(t1, t2, t3):
     sleep(t2 + .1)
 
     if _phue.is_on("Nachttischlampe"):
-        _phue.set_lights(["Schlafzimmer Hängelampe"], bri=_phue.min_bri())
-        sleep(1.0)
         scene.hell_schlafzimmer(t3, increase_only=True)
     if _phue.is_on("Stehlampe"):
-        _phue.set_lights(["Hängelampe"], bri=_phue.min_bri())
-        sleep(1.0)
         scene.hell(t3, increase_only=True)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t1', type=str, default='5m', dest='t1')
-    parser.add_argument('-t2', type=str, default='5m', dest='t2')
-    parser.add_argument('-t3', type=str, default='30m', dest='t3')
+    parser.add_argument('-f', '--fast', action='store_true', dest='fast')
+    parser.add_argument('-s', '--slow', action='store_true', dest='slow')
+    parser.add_argument('-t1', type=str, default='10m', dest='t1')
+    parser.add_argument('-t2', type=str, default='3m', dest='t2')
+    parser.add_argument('-t3', type=str, default='45m', dest='t3')
     args = parser.parse_args()
 
-    t1 = scene.convert_time_string(args.t1)
-    t2 = scene.convert_time_string(args.t2)
-    t3 = scene.convert_time_string(args.t3)
+    if args.fast:
+        t1 = "10m"
+        t2 = "3m"
+        t3 = "45m"
+    elif args.slow:
+        t1 = "15m"
+        t2 = "15m"
+        t3 = "45m"
+    else:
+        t1 = args.t1
+        t2 = args.t2
+        t3 = args.t3
+
+    t1 = scene.convert_time_string(t1)
+    t2 = scene.convert_time_string(t2)
+    t3 = scene.convert_time_string(t3)
 
     wakeup(t1, t2, t3)
 
