9013c0f24745ce16d5d0bcfc2de152eb04432bfc
Thorben Muhleder <thorbenm@me.com>
2022-07-24 21:02:04 +0200

    add t4 in wakeup; revert to arguments

--- wakeup.py
+++ wakeup.py
@@ -6,7 +6,7 @@ import argparse
 import scene
 
 
-def wakeup(t1, t2, t3):
+def wakeup(t1, t2, t3, t4):
     scene.min_schlafzimmer(increase_only=True)
     scene.min(increase_only=True)
 
@@ -24,6 +24,13 @@ def wakeup(t1, t2, t3):
     if _phue.is_on("Stehlampe"):
         scene.hell(t3, increase_only=True)
 
+    if 0.0 < t4:
+        sleep(t2 + .1)
+        if _phue.is_on("Nachttischlampe"):
+            scene.focus_schlafzimmer(t3, increase_only=True)
+        if _phue.is_on("Stehlampe"):
+            scene.focus(t3, increase_only=True)
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -32,26 +39,20 @@ def main():
     parser.add_argument('-t1', type=str, default='10m', dest='t1')
     parser.add_argument('-t2', type=str, default='3m', dest='t2')
     parser.add_argument('-t3', type=str, default='45m', dest='t3')
+    parser.add_argument('-t4', type=str, default='-1s', dest='t4')
     args = parser.parse_args()
 
-    if args.fast:
-        t1 = "1s"
-        t2 = "10m"
-        t3 = "45m"
-    elif args.slow:
-        t1 = "15m"
-        t2 = "15m"
-        t3 = "45m"
-    else:
-        t1 = args.t1
-        t2 = args.t2
-        t3 = args.t3
+    t1 = args.t1
+    t2 = args.t2
+    t3 = args.t3
+    t4 = args.t4
 
     t1 = scene.convert_time_string(t1)
     t2 = scene.convert_time_string(t2)
     t3 = scene.convert_time_string(t3)
+    t4 = scene.convert_time_string(t4)
 
-    wakeup(t1, t2, t3)
+    wakeup(t1, t2, t3, t4)
 
 
 if __name__ == '__main__':
