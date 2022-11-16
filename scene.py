282ec4e14c05038e053a88dcb8c26c8ed1592d02
Thorben Muhleder <thorbenm@me.com>
2022-11-16 14:50:17 +0000

    replace , with . so I can run from iPhone

--- scene.py
+++ scene.py
@@ -92,7 +92,7 @@ def convert_time_string(time_str):
     unit_is_minutes = False
     if time_str.endswith("m"):
         unit_is_minutes = True
-    time = float(time_str[:-1])
+    time = float(time_str[:-1].replace(",", "."))
     if unit_is_minutes:
         time *= 60
     return time
