921576f4db0ad7b4fed7637c83021befce79ea6e
Thorben Muhleder <thorbenm@me.com>
2023-12-11 11:56:20 +0100

    fakeup longer

--- fakeup.sh
+++ fakeup.sh
@@ -12,7 +12,7 @@ from time import sleep
 l = [j[0] for j in [*data.kuche_slaves, *data.bad_slaves]]
 _phue.set_lights(l, bri=_phue.min_bri(), ct=1.0)
 sleep(1.0)
-_phue.set_lights(l, bri=.75, ct=1.0, time=120.0)
-sleep(120.0)
+_phue.set_lights(l, bri=.75, ct=1.0, time=5.0*60.0)
+sleep(5.0*60.0)
 _phue.set_lights(l, bri=1.0, ct=.58, time=90.0*60.0)
 EOF
