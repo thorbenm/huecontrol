971d863b8d8e240911786eb23cba63250617e30a
Thorben Muhleder <thorbenm@me.com>
2024-01-20 09:27:21 +0100

    update fakeup with new routine

--- fakeup.sh
+++ fakeup.sh
@@ -12,7 +12,9 @@ from time import sleep
 l = [j[0] for j in [*data.kuche_slaves, *data.bad_slaves]]
 _phue.set_lights(l, bri=_phue.min_bri(), ct=1.0)
 sleep(1.0)
-_phue.set_lights(l, bri=.75, ct=1.0, time=5.0*60.0)
-sleep(5.0*60.0)
-_phue.set_lights(l, bri=1.0, ct=.58, time=90.0*60.0)
+_phue.set_lights(l, bri=.4, ct=1.0, time=60.0)
+sleep(60.0)
+_phue.set_lights(l, bri=1.0, ct=.7, time=120.0)
+sleep(120.0)
+_phue.set_lights(l, bri=1.0, ct=.48, time=90.0*60.0)
 EOF
