704d67841f3cd88d59fcd507044a0b750323541b
Thorben Muhleder <thorbenm@me.com>
2024-03-10 10:56:39 +0100

    party changes
    - all transitions 120 sec
    - kill on Stehlampe changed brightness

--- party.py
+++ party.py
@@ -1,35 +1,21 @@
 #!/usr/bin/python3
 import _phue
-import random
+from random import choice
 from time import sleep
+
 
 lamps = ["Hue Go", "Fensterlampe", "LED Streifen", "Ananas"]
 settings = [[0.559, 0.960], [0.846, 0.640], [0.649, 0.759], [0.303, 0.472], [0.319, 0.0]]
 
-counter = 0
-
-def _sleep(time):
-    global counter
-    for _ in range(int(time)):
-        if _phue.get_bri("Stehlampe") < 0.05:
-            counter += 1
-        else:
-            counter = 0
-        if 5 < counter:
-            exit()
-        sleep(1.0)
-
 
 def main():
-    while True:
-        r1 = float(random.randint(20, 60))
-        r2 = float(random.randint(20, 60))
-        lamp = random.choice(lamps)
-        setting = random.choice(settings)
-        sat = setting[0]
-        hue = setting[1]
-        _phue.set_lights(lamp, sat=sat, hue=hue, bri=.75, time=r1)
-        _sleep(r1 + r2)
+    start_bri = _phue.get_bri("Stehlampe")
+    while abs(_phue.get_bri("Stehlampe") - start_bri) < .02:
+        lamp = choice(lamps)
+        setting = choice(settings)
+        sat, hue = setting
+        _phue.set_lights(lamp, sat=sat, hue=hue, time=120)
+        sleep(120)
 
 
 if __name__ == '__main__':
