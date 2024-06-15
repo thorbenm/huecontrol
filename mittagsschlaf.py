ce80a2267afd140e6b3976af5e09e905e82ea636
Thorben Muhleder <thorbenm@me.com>
2024-06-15 14:38:50 +0200

    make transition in mittagsschlaf faster

--- mittagsschlaf.py
+++ mittagsschlaf.py
@@ -8,8 +8,8 @@ def main():
     lights = [l[0] for l in lights]
     lights = [l for l in lights if l != "Wickeltischlampe"]
     _phue.set_lights(lights, on=False)
-    _phue.set_lights("Wickeltischlampe", ct=1.0, time=60)
-    _phue.set_lights("Wickeltischlampe", bri=_phue.min_bri(), time=5*60)
+    _phue.set_lights("Wickeltischlampe", ct=1.0, time=30)
+    _phue.set_lights("Wickeltischlampe", bri=_phue.min_bri(), time=3*60)
 
 
 if __name__ == '__main__':
